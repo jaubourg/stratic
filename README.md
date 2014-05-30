764bf5e0e9db1c44774f6b3ddcd316df491a4a1a
jaubourg <j@ubourg.net>
2014-05-31 00:04:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Build Status](https://travis-ci.org/jaubourg/stratic.svg?branch=master)](https://travis-ci.org/jaubourg/stratic)
+[![Coverage Status](https://img.shields.io/coveralls/jaubourg/stratic.svg)](https://coveralls.io/r/jaubourg/stratic)
+[![Dependency Status](https://david-dm.org/jaubourg/stratic.svg)](https://david-dm.org/jaubourg/stratic)
+[![devDependency Status](https://david-dm.org/jaubourg/stratic/dev-status.svg)](https://david-dm.org/jaubourg/stratic#info=devDependencies)
+[![Gittip](https://img.shields.io/gittip/jaubourg.svg)](https://www.gittip.com/jaubourg/)
+
+[![NPM](https://nodei.co/npm/stratic.png?downloads=true&stars=true)](https://www.npmjs.org/package/stratic)
+# stratic
+
+Yet another static site generator.
+
+## License
+
+Copyright (c) 2012 - 2014 [Julian Aubourg](mailto:j@ubourg.net)
+Licensed under the [MIT license](https://raw.githubusercontent.com/jaubourg/stratic/master/LICENSE-MIT).
---
+++ index.js
@@ -0,0 +1,89 @@
+"use strict";
+
+module.export = function( __dirname ) {
+
+	var assign = require( "lodash.assign" );
+	var copyDir = require( "./lib/copyDir" );
+	var dirmap = require( "./lib/dirmap" );
+	var fs = require( "fs" );
+	var jade = require( "./lib/jade" );
+	var min = require( "html-minifier").minify;
+	var path = require( "path" );
+	var template = require( "lodash.template" );
+	var sass = require( "node-sass" );
+
+	var basedir = __dirname + "/..";
+	var data = dirmap( basedir + "/layout/data", dirmap( basedir + "/data", {
+		basedir: basedir
+	} ) );
+
+	copyDir( {
+		src: basedir + "/layout",
+		dest: basedir + "/compiled-layout",
+		".scss": function( content ) {
+			return template( content, data, {
+				evaluate: /\/\*%([\s\S]+?)%\*\//g,
+				interpolate: /\/\*%=([\s\S]+?)%\*\//g,
+				escape: /\/\*%-([\s\S]+?)%\*\//g
+			} );
+		}
+	}, function() {
+
+		copyDir( {
+			src: basedir + "/content",
+			dest: basedir + "/dist",
+			block: function( filename ) {
+				return filename[ 0 ] === "_";
+			},
+			".jade": function( content, filename ) {
+				var basename = path.basename( filename, ".jade" );
+				var dirname = path.dirname( filename );
+				copyDir( {
+					src: path.resolve( dirname, "_" + basename, "resources" ),
+					dest: basedir + "/dist/resources"
+				} );
+				return {
+					filename: basename + ".html",
+					content: min(
+						jade.render( "extends " + path.relative( dirname, basedir + "/compiled-layout/layout" ) + "\n\n" + content,
+						assign( {}, data, {
+							filename: filename
+						} )
+					), {
+						minifyCSS: true,
+						minifyJS: true,
+						removeComments: true,
+						removeCommentsFromCDATA: true,
+						removeCDATASectionsFromCDATA: true,
+						collapseWhitespace: true,
+						collapseBooleanAttributes: true,
+						removeAttributeQuotes: true,
+						removeRedundantAttributes: true,
+						useShortDoctype: true,
+						removeEmptyAttributes: true,
+						removeOptionalTags: true,
+						removeEmptyElements: false
+					} ).replace( /\s([!?;:])/g, function( _, $1 ) {
+						return "&nbsp;" + $1;
+					} )
+				};
+			}
+		}, function() {
+			copyDir( {
+				src: basedir + "/compiled-layout/resources",
+				dest: basedir + "/dist/resources"
+			} );
+			sass.render({
+				file: basedir + "/compiled-layout/layout.scss",
+				success: function( css ) {
+					fs.writeFile( basedir + "/dist/layout.css", css );
+				},
+				error: function(error) {
+					console.error( error );
+				},
+				outputStyle: "compressed"
+			} );
+		} );
+	} );
+
+};
